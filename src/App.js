563b9aca9172c51678242a2347bd992cf0a4e787
KonstaKozh <konstakozh@yandex.ru>
2022-10-23 21:27:13 +0400

    create app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {useForm} from "react-hook-form";
+
+function App() {
+    const {register, handleSubmit} = useForm()
+  return (
+    <div className="App">
+      Timetable
+        <form >
+            
+        </form>
+    </div>
+  );
+}
+
+export default App;
